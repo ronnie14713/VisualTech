0bb88ff1d1d07689af6268dc1a7250c9f8199b63
Ronnie <ronnie@Ronniede-MacBook-Pro.local>
2018-02-06 00:16:19 -0500

    nav bar done

---
+++ number.js
@@ -0,0 +1,25 @@
+
+
+
+var squared = function(x) {
+	var squaredNumber = x * x;
+	return squaredNumber;
+
+};
+
+
+var plus10 = function(x) {
+	var newNumber = x + 10;
+	return newNumber;
+
+};
+
+var numbers = {
+	"squared": squared,
+	"plus10": plus10
+};
+
+
+
+
+
